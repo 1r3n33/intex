20bf8d27eacf7bd0c412cc93f35f2f342e25cd59
Irenee <icaroulle@gmail.com>
2020-10-31 22:54:21 +0100

    Add wallet connection to react-app

--- app/react-app/src/App.js
+++ app/react-app/src/App.js
@@ -1,21 +1,48 @@
+import React from 'react';
+
 import './App.css';
 import Web3 from 'web3'
 
-function App() {
-  if (window.ethereum) {
-    window.web3 = new Web3(window.ethereum);
-    window.ethereum.enable();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { connected: false };
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Please connect to your wallet.
-        </p>
-      </header>
-    </div>
-  );
+  render() {
+    if (!this.state.connected)
+    {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        window.ethereum.enable()
+          .then(_ => {
+            this.setState({ connected: true });
+          });
+      }
+
+      return (
+        <div className="App">
+          <header className="App-header">
+            <p>
+              Please connect to your wallet.
+            </p>
+          </header>
+        </div>
+      );
+    }
+    else
+    {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <p>
+              Welcome to Intex.
+            </p>
+          </header>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
