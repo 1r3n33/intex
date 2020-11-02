fca4c7144f438b540e9693fc69fd4a498ab3ca7f
Irenee <icaroulle@gmail.com>
2020-11-02 19:15:45 +0100

    Add component to connect to wallet

--- app/react-app/src/App.js
+++ app/react-app/src/App.js
@@ -3,32 +3,45 @@ import React from 'react';
 import './App.css';
 import Web3 from 'web3'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { connected: false };
+class ConnectToWallet extends React.Component {
+  componentDidMount() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      window.ethereum.enable()
+        .then(_ => {
+          this.props.onConnectedToWallet(true);
+        });
+    }
   }
 
   render() {
-    if (!this.state.connected)
-    {
-      if (window.ethereum) {
-        window.web3 = new Web3(window.ethereum);
-        window.ethereum.enable()
-          .then(_ => {
-            this.setState({ connected: true });
-          });
-      }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Please connect to your wallet.
+          </p>
+        </header>
+      </div>
+    );
+  }
+};
 
-      return (
-        <div className="App">
-          <header className="App-header">
-            <p>
-              Please connect to your wallet.
-            </p>
-          </header>
-        </div>
-      );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onConnectedToWallet = this.onConnectedToWallet.bind(this);
+    this.state = { connectedToWallet: false };
+  }
+
+  onConnectedToWallet(connected) {
+    this.setState( { connectedToWallet: connected } );
+  }
+
+  render() {
+    if (!this.state.connectedToWallet)
+    {
+      return <ConnectToWallet onConnectedToWallet={this.onConnectedToWallet}/>
     }
     else
     {
