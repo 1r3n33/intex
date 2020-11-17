a2e278c2301644284bb0bd72fd23090907d3805b
Irenee <icaroulle@gmail.com>
2020-11-17 22:18:05 +0100

    Prepare component to add brand safety

--- app/react-app/src/components/ProviderDashboard.js
+++ app/react-app/src/components/ProviderDashboard.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import Table from 'react-bulma-components/lib/components/table';
+import { Field, Control, Input } from 'react-bulma-components/lib/components/form';
+import Button from 'react-bulma-components/lib/components/button';
 
 class ProviderDashboard extends React.Component {
   render() {
@@ -11,6 +15,23 @@ class ProviderDashboard extends React.Component {
           {this.props.user.address}
           <br/>
           {this.props.web3.utils.hexToString(this.props.user.provider.name)}
+          <Table>
+            <thead>
+              <tr>
+                <th>URL</th>
+                <th>Brand Safety Classification</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+          </Table>
+          <Field kind='addons'>
+            <Control>
+              <Input placeholder="URL"/>
+            </Control>
+            <Control>
+              <Button color='success'>Add</Button>
+            </Control>
+          </Field>
         </header>
       </div>
     );
