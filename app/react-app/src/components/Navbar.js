f5d992ac8d896c995eebee090d245cbf1850a6da
Irenee <icaroulle@gmail.com>
2020-12-31 15:54:08 +0100

    Create Navbar component

---
+++ app/react-app/src/components/Navbar.js
@@ -0,0 +1,106 @@
+import React from "react";
+import "react-bulma-components/dist/react-bulma-components.min.css";
+import { default as BulmaNavbar } from "react-bulma-components/lib/components/navbar";
+import Modal from "react-bulma-components/lib/components/modal";
+import Section from "react-bulma-components/lib/components/section";
+import Button from "react-bulma-components/lib/components/button";
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onBuyModalOpen = this.onBuyModalOpen.bind(this);
+    this.onBuyModalClose = this.onBuyModalClose.bind(this);
+    this.onBuyIntexButtonClick = this.onBuyIntexButtonClick.bind(this);
+
+    this.state = {
+      showBuyModal: false,
+    };
+  }
+
+  onBuyModalOpen() {
+    this.setState({
+      showBuyModal: true,
+    });
+  }
+
+  onBuyModalClose() {
+    this.setState({
+      showBuyModal: false,
+    });
+  }
+
+  async onBuyIntexButtonClick() {
+    try {
+      // Buy INTX
+      await this.props.intex.getTokens({
+        from: this.props.user.address,
+        value: this.props.web3.utils.toWei("1"),
+      });
+
+      // Get new balance
+      const balance = await this.props.intex.balanceOf(this.props.user.address);
+
+      this.setState({
+        showBuyModal: false,
+      });
+      this.props.onBalanceChange(balance);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Modal show={this.state.showBuyModal} onClose={this.onBuyModalClose}>
+          <Modal.Content>
+            <Section style={{ backgroundColor: "white" }}>
+              <div>Get 1,000,000 INTX</div>
+              <Button color="success" onClick={this.onBuyIntexButtonClick}>
+                Buy
+              </Button>
+            </Section>
+          </Modal.Content>
+        </Modal>
+        <BulmaNavbar>
+          <BulmaNavbar.Menu>
+            <BulmaNavbar.Container>
+              <BulmaNavbar.Item>
+                <h1 className="title has-text-weight-normal">
+                  Welcome to <span className="has-text-weight-bold">Intex</span>
+                </h1>
+              </BulmaNavbar.Item>
+            </BulmaNavbar.Container>
+            <BulmaNavbar.Container position="end">
+              <BulmaNavbar.Item>
+                <div>
+                  <h1 className="title is-4">
+                    {this.props.web3.utils.hexToString(
+                      this.props.user.provider.name
+                    )}
+                  </h1>
+                  <h2 className="subtitle is-6">{this.props.user.address}</h2>
+                </div>
+              </BulmaNavbar.Item>
+              <BulmaNavbar.Item>
+                <div>
+                  <h1 className="title is-4" onClick={this.onBuyModalOpen}>
+                    {this.props.balance
+                      ? this.props.web3.utils.fromWei(this.props.balance)
+                      : "-"}
+                  </h1>
+                  <h2 className="subtitle is-6" onClick={this.onBuyModalOpen}>
+                    INTX
+                  </h2>
+                </div>
+              </BulmaNavbar.Item>
+            </BulmaNavbar.Container>
+          </BulmaNavbar.Menu>
+        </BulmaNavbar>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
--- app/react-app/src/components/ProviderDashboard.js
+++ app/react-app/src/components/ProviderDashboard.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import 'react-bulma-components/dist/react-bulma-components.min.css';
-import Navbar from 'react-bulma-components/lib/components/navbar';
-import Columns from 'react-bulma-components/lib/components/columns';
-import { Field, Control, Input, Select } from 'react-bulma-components/lib/components/form';
-import Button from 'react-bulma-components/lib/components/button';
-import Table from 'react-bulma-components/lib/components/table';
-import Modal from 'react-bulma-components/lib/components/modal';
-import Section from 'react-bulma-components/lib/components/section';
-import normalizeUrl from 'normalize-url';
-import BrandSafetyCategories from './BrandSafety/Categories';
+import React from "react";
+import "react-bulma-components/dist/react-bulma-components.min.css";
+import Columns from "react-bulma-components/lib/components/columns";
+import {
+  Field,
+  Control,
+  Input,
+  Select,
+} from "react-bulma-components/lib/components/form";
+import Button from "react-bulma-components/lib/components/button";
+import Table from "react-bulma-components/lib/components/table";
+import normalizeUrl from "normalize-url";
+import BrandSafetyCategories from "./BrandSafety/Categories";
+import Navbar from "./Navbar";
 
 class ProviderDashboard extends React.Component {
   constructor(props) {
     super(props);
 
-    this.onBuyIntexButtonClick = this.onBuyIntexButtonClick.bind(this);
     this.onUrlInputValueChange = this.onUrlInputValueChange.bind(this);
     this.onAddUrlButtonClick = this.onAddUrlButtonClick.bind(this);
-    this.onBrandSafetyCategoriesSelectionChange = this.onBrandSafetyCategoriesSelectionChange.bind(this);
-    this.onBuyModalOpen = this.onBuyModalOpen.bind(this);
-    this.onBuyModalClose = this.onBuyModalClose.bind(this);
+    this.onBrandSafetyCategoriesSelectionChange = this.onBrandSafetyCategoriesSelectionChange.bind(
+      this
+    );
+    this.onBalanceChange = this.onBalanceChange.bind(this);
 
     this.state = {
-      url: '',
+      url: "",
       brandSafetyCategories: [],
       dataIntelligences: [],
       balance: undefined,
-      showBuyModal: false
     };
 
     this.brandSafetyCategories = new BrandSafetyCategories();
@@ -36,33 +38,17 @@ class ProviderDashboard extends React.Component {
     const address = this.props.user.address;
 
     try {
-      const dataIntelligences = await this.props.exchange.getDataIntelligences(address);
-
-      const balance = await this.props.intex.balanceOf(address);
-
-      this.setState({
-        dataIntelligences: dataIntelligences,
-        balance: balance
-      });
-
-    } catch (ex) {
-      console.log(ex);
-    }
-  }
-
-  async onBuyIntexButtonClick() {
-    try {
-      // Buy INTX
-      await this.props.intex.getTokens({ from: this.props.user.address, value: this.props.web3.utils.toWei('1') });
+      const dataIntelligences = await this.props.exchange.getDataIntelligences(
+        address
+      );
 
       // Get new balance
       const balance = await this.props.intex.balanceOf(this.props.user.address);
 
       this.setState({
+        dataIntelligences: dataIntelligences,
         balance: balance,
-        showBuyModal: false
       });
-
     } catch (ex) {
       console.log(ex);
     }
@@ -73,78 +59,88 @@ class ProviderDashboard extends React.Component {
   }
 
   onBrandSafetyCategoriesSelectionChange(e) {
-    const categories = Array.from(e.target.selectedOptions, option => option.value);
+    const categories = Array.from(
+      e.target.selectedOptions,
+      (option) => option.value
+    );
     this.setState({ brandSafetyCategories: categories });
   }
 
   async onAddUrlButtonClick(e) {
     const address = this.props.user.address;
 
-    const normalizedUrl = normalizeUrl(this.state.url, { stripProtocol: true, stripHash: true });
+    const normalizedUrl = normalizeUrl(this.state.url, {
+      stripProtocol: true,
+      stripHash: true,
+    });
 
     const url = this.props.web3.utils.asciiToHex(normalizedUrl);
 
-    const bytes = this.brandSafetyCategories.toBytes(this.state.brandSafetyCategories);
+    const bytes = this.brandSafetyCategories.toBytes(
+      this.state.brandSafetyCategories
+    );
 
     try {
       const price = await this.props.exchange.addDataIntelligencePrice();
 
       // Before using Intex token to add Data Intelligence, we must increase allowance of the Exchange contract.
-      await this.props.intex.increaseAllowance(this.props.exchange.address, price, { from: address });
+      await this.props.intex.increaseAllowance(
+        this.props.exchange.address,
+        price,
+        { from: address }
+      );
 
       // Add data intelligence.
-      await this.props.exchange.addDataIntelligence(url, 0, bytes, { from: address });
-
-      // Get data intelligences to refresh state.
-      const dataIntelligences = await this.props.exchange.getDataIntelligences(address);
-
-      // Get new balance
-      const balance = await this.props.intex.balanceOf(address);
-
-      this.setState({
-        url: '',
-        brandSafetyCategories: [],
-        dataIntelligences: dataIntelligences,
-        balance: balance
+      await this.props.exchange.addDataIntelligence(url, 0, bytes, {
+        from: address,
       });
 
+      // Get data intelligences to refresh state.
+      const dataIntelligences = await this.props.exchange.getDataIntelligences(
+        address
+      );
+
+      // Get new balance
+      const balance = await this.props.intex.balanceOf(this.props.user.address);
+
+      this.setState({
+        url: "",
+        brandSafetyCategories: [],
+        dataIntelligences: dataIntelligences,
+        balance: balance,
+      });
     } catch (ex) {
       console.log(ex);
     }
   }
 
-  onBuyModalOpen() {
-    this.setState({
-      showBuyModal: true
-    });
-  }
-
-  onBuyModalClose() {
-    this.setState({
-      showBuyModal: false
-    });
+  onBalanceChange(balance) {
+    this.setState({ balance: balance });
   }
 
   renderSelectOptions() {
     const keys = Object.keys(this.brandSafetyCategories.categories);
-    return keys.map(k => {
+    return keys.map((k) => {
       return (
-        <option key={k} value={k}>{this.brandSafetyCategories.categories[k]}</option>
+        <option key={k} value={k}>
+          {this.brandSafetyCategories.categories[k]}
+        </option>
       );
     });
   }
 
   renderTableBody() {
     return this.state.dataIntelligences.map((dataIntelligence, index) => {
-
-      const ids = this.brandSafetyCategories.fromHexadecimalString(dataIntelligence.data);
-      const names = ids.map(id => this.brandSafetyCategories.categories[id]);
+      const ids = this.brandSafetyCategories.fromHexadecimalString(
+        dataIntelligence.data
+      );
+      const names = ids.map((id) => this.brandSafetyCategories.categories[id]);
 
       return (
         <tr key={index}>
           <td>{index}</td>
           <td>{this.props.web3.utils.hexToAscii(dataIntelligence.source)}</td>
-          <td>{names.join(';')}</td>
+          <td>{names.join(";")}</td>
           <td>{dataIntelligence.timestamp}</td>
         </tr>
       );
@@ -153,57 +149,41 @@ class ProviderDashboard extends React.Component {
 
   render() {
     return (
-      <div className='App'>
-        <Modal show={this.state.showBuyModal} onClose={this.onBuyModalClose}>
-          <Modal.Content>
-            <Section style={{ backgroundColor: 'white' }}>
-              <div>
-                Get 1,000,000 INTX
-              </div>
-              <Button color='success' onClick={this.onBuyIntexButtonClick}>Buy</Button>
-            </Section>
-          </Modal.Content>
-        </Modal>
-        <Navbar>
-          <Navbar.Menu>
-            <Navbar.Container>
-              <Navbar.Item>
-                <h1 className='title has-text-weight-normal'>Welcome to <span className='has-text-weight-bold'>Intex</span></h1>
-              </Navbar.Item>
-            </Navbar.Container>
-            <Navbar.Container position='end'>
-              <Navbar.Item>
-                <div>
-                  <h1 className='title is-4'>{this.props.web3.utils.hexToString(this.props.user.provider.name)}</h1>
-                  <h2 className="subtitle is-6">{this.props.user.address}</h2>
-                </div>
-              </Navbar.Item>
-              <Navbar.Item>
-                <div>
-                  <h1 className='title is-4' onClick={this.onBuyModalOpen}>{this.state.balance ? this.props.web3.utils.fromWei(this.state.balance) : '-'}</h1>
-                  <h2 className="subtitle is-6" onClick={this.onBuyModalOpen}>INTX</h2>
-                </div>
-              </Navbar.Item>
-            </Navbar.Container>
-          </Navbar.Menu>
-        </Navbar>
-        <header className='App-header'>
+      <div className="App">
+        <Navbar
+          web3={this.props.web3}
+          intex={this.props.intex}
+          user={this.props.user}
+          balance={this.state.balance}
+          onBalanceChange={this.onBalanceChange}
+        />
+        <header className="App-header">
           <br />
           <Columns vCentered={true}>
             <Columns.Column size={7}>
               <Field>
                 <Control>
-                  <Input placeholder='URL' value={this.state.url} onChange={this.onUrlInputValueChange} />
+                  <Input
+                    placeholder="URL"
+                    value={this.state.url}
+                    onChange={this.onUrlInputValueChange}
+                  />
                 </Control>
               </Field>
             </Columns.Column>
             <Columns.Column size={4}>
-              <Select multiple={true} value={this.state.brandSafetyCategories} onChange={this.onBrandSafetyCategoriesSelectionChange}>
+              <Select
+                multiple={true}
+                value={this.state.brandSafetyCategories}
+                onChange={this.onBrandSafetyCategoriesSelectionChange}
+              >
                 {this.renderSelectOptions()}
               </Select>
             </Columns.Column>
             <Columns.Column size={1}>
-              <Button color='success' onClick={this.onAddUrlButtonClick}>Add</Button>
+              <Button color="success" onClick={this.onAddUrlButtonClick}>
+                Add
+              </Button>
             </Columns.Column>
           </Columns>
           <Table>
@@ -215,9 +195,7 @@ class ProviderDashboard extends React.Component {
                 <th>Timestamp</th>
               </tr>
             </thead>
-            <tbody>
-              {this.renderTableBody()}
-            </tbody>
+            <tbody>{this.renderTableBody()}</tbody>
           </Table>
         </header>
       </div>
